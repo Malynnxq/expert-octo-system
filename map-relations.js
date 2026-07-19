91b8ce945ca6a984abb1b5b521e48e47ee728a52
Malynnxq <128480501+Malynnxq@users.noreply.github.com>
2026-07-19 21:43:45 +0200

    Add semantic relation data for recursive functions

---
+++ map-relations.js
@@ -0,0 +1,16 @@
+window.MAP_R=[
+['procedure','mapping','realisiert'],['algorithm','procedure','ist Form von'],['effective','algorithm','charakterisiert'],['total','termination','terminiert immer'],['partial','termination','nicht zwingend'],['partial','undefined','kann liefern'],
+['decisionProblem','characteristic','dargestellt durch'],['language','characteristic','entschieden durch'],['primeLanguage','decidableLanguage','ist'],['semiLanguage','acceptedLanguage','akzeptiert von'],
+['head','tape','liest/schreibt'],['control','transition','wendet an'],['transition','configuration','erzeugt nächste'],['startConfig','closure','startet'],['closure','acceptedLanguage','führt bei Akzeptanz zu'],['acceptedLanguage','semiLanguage','ist i.A.'],
+['multiTape','singleTape','simuliert durch'],['simulation','equivalence','beweist'],['universal','tm','simuliert jede'],['goedelization','universal','ermöglicht'],
+['while','whileComp','definiert'],['goto','gotoComp','definiert'],['loop','loopComp','definiert'],
+['whileComp','turingComputable','ist Teilmenge von'],['while','goto','wird simuliert durch'],['goto','while','wird simuliert durch'],['goto','tm','simuliert'],['tm','goto','wird simuliert durch'],
+['whileComp','gotoComp','gleichmächtig'],['gotoComp','turingComputable','gleichmächtig'],['whileComp','turingComputable','gleichmächtig'],
+['kleene','whileComp','normalisiert'],['loopComp','totalOnly','impliziert'],['whileLoop','loop','nicht ausdrückbar in'],
+['ackDef','ackermann','definiert'],['ackermann','ackTotal','ist'],['ackermann','ackTuring','ist'],['ackermann','ackWhile','ist'],['ackermann','ackNotLoop','ist'],['ackMono','loopBoundFn','ermöglicht Dominanzbeweis'],['loopBoundFn','ackNotLoop','beweist'],['ackNotLoop','loopComp','liegt außerhalb von'],['ackWhile','whileComp','ist Beispiel für'],['ackTotal','totalGap','zeigt Existenz von'],['totalGap','totalOnly','ist strikt größer als'],
+['loopComp','whileComp','ist Teilmenge von'],['assignment','while','Grundsyntax von'],['assignment','goto','Grundsyntax von'],['assignment','loop','Grundsyntax von'],
+['constantFunctions','primitiveRecursive','ist Basis von'],['projections','primitiveRecursive','ist Basis von'],['successor','primitiveRecursive','ist Basis von'],['compositionOp','primitiveRecursive','schließt ab'],['primitiveRecursion','primitiveRecursive','schließt ab'],['primitiveRecursive','loopComp','ist genau gleichmächtig'],['primitiveRecursive','ackermann','enthält nicht'],
+['muOperator','mu','erweitert zu'],['muOperator','partial','kann erzeugen'],['muUndefined','undefined','entspricht'],['primitiveRecursive','mu','ist Teilklasse von'],['mu','whileComp','ist genau gleichmächtig'],['mu','gotoComp','ist genau gleichmächtig'],['mu','turingComputable','ist genau gleichmächtig'],['muNormalForm','mu','stellt jede Funktion dar'],['normalP','muNormalForm','verwendet'],['normalQ','muOperator','wird minimiert durch'],
+['church','formal','identifiziert mit'],['church','intuitive','identifiziert mit'],['haltingProblem','undecidableSet','ist Beispiel für'],['reduction','undecidableSet','überträgt'],
+['classes','complexity','strukturieren'],['pnp','classes','vergleicht'],['npcomplete','pnp','zentral für']
+];
